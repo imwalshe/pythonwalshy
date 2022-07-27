ff8290f2db8042a7b1ad16bf4b0834dac5297aee
imwalshe <imwalshe@users.noreply.github.com>
2022-07-27 09:51:41 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="imwalshe"
+original_name="pythonwalshy"
+original_urlname="pythonwalshy"
+original_description="Awesome pythonwalshy created by imwalshe"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv pythonwalshy $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- pythonwalshy/cli.py
+++ pythonwalshy/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for pythonwalshy project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m pythonwalshy` and `$ pythonwalshy `.
 
     This is your program's entry point.
 
